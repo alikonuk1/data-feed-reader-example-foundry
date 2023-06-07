5d68108a0625722e39c010fd0a1a9a2b473fd6cc
alikonuk1 <alikonuk1@gmail.com>
2023-06-07 13:44:54 +0300

    wip

--- src/DataFeedReaderExample.sol
+++ src/DataFeedReaderExample.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.17;
 
-import "contracts/contracts/v0.8/interfaces/IProxy.sol";
+import "./interfaces/IProxy.sol";
 
 contract DataFeedReaderExample is Ownable {
     // This contract reads from a single proxy. Your contract can read from
